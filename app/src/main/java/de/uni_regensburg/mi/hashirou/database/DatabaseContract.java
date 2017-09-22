f90514fc9d782b74016cb28c9d89c8be48de0ac3
Sebastian Keck <sebastiankeck7@gmail.com>
2017-09-22 12:18:10 +0200

    Update DatabaseContract.java

--- app/src/main/java/de/uni_regensburg/mi/hashirou/database/DatabaseContract.java
+++ app/src/main/java/de/uni_regensburg/mi/hashirou/database/DatabaseContract.java
@@ -1,8 +1,21 @@
 package de.uni_regensburg.mi.hashirou.database;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by k3k5 on 28.07.17.
  */
 
 public class DatabaseContract {
+
+    private DatabaseContract() {}
+
+    public static class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "run";
+        public static final String COLUMN_NAME_TIMESTAMP = "timestamp";
+        public static final String COLUMN_NAME_LOCATION_LAT = "location_lat";
+        public static final String COLUMN_NAME_LOCATION_LNG = "location_lng";
+        public static final String COLUMN_NAME_CURRENT_SPEED = "speed";
+        public static final String COLUMN_NAME_CURRENT_HEIGHT = "height";
+    }
 }
